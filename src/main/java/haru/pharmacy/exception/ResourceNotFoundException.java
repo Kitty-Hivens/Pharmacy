8f6b86489f3c3d91596cb54df4cfc7c8d1345817
GoldyL <vitaliyfnaf@gmail.com>
2025-12-05 14:48:44 +0100

    feat: add resource not found exception

---
+++ src/main/java/haru/pharmacy/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package haru.pharmacy.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
